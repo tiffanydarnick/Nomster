c93af139840b21c9cc99b89f8c01835fcd88bc32
Tiffany <tdarnick@outlook.com>
2015-10-06 23:56:15 +0000

    lesson29

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
+	geocoded_by :address
+	after_validation :geocode
+
 	validates :name, :presence => true
 	validates :address, :presence => true
 	validates :description, :presence => true
