d70dccba317160ba013781918f22beeb93147fd2
Tiffany <tdarnick@outlook.com>
2015-10-11 16:31:46 +0000

    photo has many

--- db/migrate/20151011161116_create_photos.rb
+++ db/migrate/20151011161116_create_photos.rb
@@ -3,7 +3,13 @@ class CreatePhotos < ActiveRecord::Migration
     create_table :photos do |t|
     	t.text :caption
 
-      t.timestamps
+    	t.integer :user_id
+    	t.integer :place_id
+
+      	t.timestamps
     end
+
+    	add_index :caption, [:user_id, :place_id]
+    	add_index :caption, :place_id
   end
 end
